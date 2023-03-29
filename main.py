0370ac7c52e927579a3e5cbdbb5e2ff969fc1c68
Sk41p31 <mibu989@gmail.com>
2023-03-29 19:48:41 +0200

    Changed data file format from .txt to .json. Added searching functionality. Added Error handling.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import json
 from tkinter import *
 from tkinter import messagebox
 import random
 import pyperclip
+import json
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
@@ -38,18 +40,50 @@ def save_to_file():
     if len(website_entry.get()) * len(email_entry.get()) * len(passwd_entry.get()) == 0:
         messagebox.showerror(title="Error", message="Please don't leave any fields empty!")
     else:
-        line_to_write = website_entry.get() + " | " + email_entry.get() + " | " + passwd_entry.get() + "\n"
+        data_to_write = {
+            website_entry.get(): {
+                "email": email_entry.get(),
+                "password": passwd_entry.get()
+            }
+        }
+
         msg = f"Do you want to save? \nEmail/Username: {email_entry.get()} \nPassword: {passwd_entry.get()}"
         is_ok = messagebox.askokcancel(title=f"Website: {website_entry.get()}", message=msg)
 
         if is_ok:
-            with open("passwords.txt", 'a') as file:
-                file.write(line_to_write)
+            try:
+                with open("passwords.json", 'r') as file:
+                    data = json.load(file)
+                    data.update(data_to_write)
+            except FileNotFoundError:
+                data = data_to_write
+            with open("passwords.json", 'w') as file:
+                json.dump(data, file, indent=4)
+
             website_entry.delete(0, END)
             passwd_entry.delete(0, END)
 
+# ---------------------------- SEARCH FOR PASSWORD ------------------------------- #
+
+
+def find_password():
+    website = website_entry.get()
+    try:
+        with open("passwords.json", "r") as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        msg = f"There is no data file!"
+        messagebox.showerror(title=f"Website: {website}", message=msg)
+    else:
+        if website in data:
+            msg = f"Email/Username: {data[website]['email']} \nPassword: {data[website]['password']}"
+            messagebox.showinfo(title=f"Website: {website}", message=msg)
+        else:
+            msg = f"Data does not exist for given website!"
+            messagebox.showwarning(title=f"Website: {website}", message=msg)
 
 # ---------------------------- UI SETUP ------------------------------- #
+
 
 window = Tk()
 window.title("Password Manager")
@@ -64,21 +98,24 @@ Label(text="Website:").grid(column=0, row=1)
 Label(text="Email/Username:").grid(column=0, row=2)
 Label(text="Password:").grid(column=0, row=3)
 
-website_entry = Entry(width=53)
-website_entry.grid(column=1, row=1, columnspan=2)
+website_entry = Entry(width=33)
+website_entry.grid(column=1, row=1)
 website_entry.focus()
 
 email_entry = Entry(width=53)
 email_entry.insert(0, "mibu989@gmail.com")
 email_entry.grid(column=1, row=2, columnspan=2)
 
-passwd_entry = Entry(width=34)
+passwd_entry = Entry(width=33)
 passwd_entry.grid(column=1, row=3)
 
-gen_passwd_btn = Button(text="Generate Password", command=generate_passwd)
+gen_passwd_btn = Button(text="Generate Password", width=16, command=generate_passwd)
 gen_passwd_btn.grid(column=2, row=3)
 
 add_btn = Button(text="Add", width=45, command=save_to_file)
 add_btn.grid(column=1, row=4, columnspan=2)
+
+search_btn =  Button(text="Search", width=16, command=find_password)
+search_btn.grid(column=2, row=1)
 
 window.mainloop()
